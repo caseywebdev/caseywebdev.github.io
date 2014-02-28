8beda13243b27d6ac963431ce0e5285a75ede8e6
Casey Foster <c@sey.me>
2014-02-28 13:31:50 -0600

    Remove PIXI, use puppies

--- client/components/index.es6.jsx
+++ client/components/index.es6.jsx
@@ -3,7 +3,6 @@
 import _ from 'underscore';
 import config from 'config';
 import Box2D from 'box2d';
-import PIXI from 'pixi';
 import React from 'react';
 
 var requestAnimationFrame = window.requestAnimationFrame;
@@ -13,9 +12,6 @@ export default React.createClass({
   componentWillMount: function () {
     this.mouseIsDown = false;
     this.mouseX = this.mouseY = 0;
-    this.renderer = new PIXI.autoDetectRenderer(400, 300);
-    this.stage = new PIXI.Stage(0xFFFF00);
-    this.container = new PIXI.DisplayObjectContainer();
     this.bodies = [];
     var gravity = new Box2D.b2Vec2(0, config.gravity);
     window.world = this.world = new Box2D.b2World(gravity);
@@ -40,7 +36,6 @@ export default React.createClass({
       bodyDef.set_type(Box2D.b2_dynamicBody);
       var body = this.body = this.world.CreateBody(bodyDef);
       Box2D.destroy(bodyDef);
-      var gfx = new PIXI.Graphics();
       var pos = body.GetPosition();
       pos.Set(Math.random() * 20, Math.random() * 15);
       body.SetTransform(pos, 0);
@@ -62,42 +57,11 @@ export default React.createClass({
       body.CreateFixture(fixtureDef);
       Box2D.destroy(fixtureDef);
 
-      gfx.beginFill(Math.random() * 0xFFFFFF);
-      // gfx.drawRect(
-      //   -1 * config.ptm,
-      //   -1 * config.ptm,
-      //   2 * config.ptm,
-      //   2 * config.ptm
-      // );
-      gfx.drawEllipse(0, 0, .5 * config.ptm, .5 * config.ptm);
-      gfx.lineStyle(2, Math.random() * 0xFFFFFF);
-      gfx.moveTo(0, 0);
-      gfx.lineTo(.5 * config.ptm, 0);
-
-      this.container.addChild(gfx);
-
-      this.bodies.push({b2: body, pixi: gfx});
+      this.bodies.push(body);
     }, this);
-
-    // var dog = new PIXI.Graphics();
-    // dog.beginFill(Math.random() * 0xFFFFFF);
-    // // gfx.drawRect(
-    // //   -1 * config.ptm,
-    // //   -1 * config.ptm,
-    // //   2 * config.ptm,
-    // //   2 * config.ptm
-    // // );
-    // dog.drawEllipse(0, 0, 1 * config.ptm, 1 * config.ptm);
-    // dog.lineStyle(2, Math.random() * 0xFFFFFF);
-    // dog.moveTo(0, 0);
-    // dog.lineTo(1 * config.ptm, 0);
-    // this.container.addChild(dog);
-
-    this.stage.addChild(this.container);
   },
 
   componentDidMount: function () {
-    this.getDOMNode().appendChild(this.renderer.view);
     this.lastStep = Date.now();
     this.step();
     this.redraw();
@@ -113,21 +77,9 @@ export default React.createClass({
     var dt = (now - this.lastStep) / 1000;
     this.lastStep = now;
     this.world.Step(dt, 8, 3);
-    // if (this.mouseIsDown) {
-    //   var mX = this.mouseX / config.ptm;
-    //   var mY = this.mouseY / config.ptm;
-    //   var dX = x - mX;
-    //   var dY = y - mY;
-    //   var d = Math.sqrt(Math.pow(dX, 2) + Math.pow(dY, 2));
-    //   var vector = new Box2D.b2Vec2(-(dX / d) * 100, -(dY / d) * 100);
-    //   body.b2.ApplyForce(vector, body.b2.GetWorldCenter());
-    //   Box2D.destroy(vector);
-    // }
-    // this.dog.position.x = this.mouseX;
-    // this.dog.position.y = this.mouseY;
     _.each(this.bodies, function (body) {
-      var x = body.b2.GetPosition().get_x();
-      var y = body.b2.GetPosition().get_y();
+      var x = body.GetPosition().get_x();
+      var y = body.GetPosition().get_y();
       var mX = this.mouseX / config.ptm;
       var mY = this.mouseY / config.ptm;
       var dX = x - mX;
@@ -135,18 +87,15 @@ export default React.createClass({
       var d = Math.sqrt(Math.pow(dX, 2) + Math.pow(dY, 2));
       if (d < 5) {
         var vector = new Box2D.b2Vec2((dX / d) * 100, (dY / d) * 100);
-        body.b2.ApplyForce(vector, body.b2.GetWorldCenter());
+        body.ApplyForce(vector, body.GetWorldCenter());
         Box2D.destroy(vector);
       }
-      body.pixi.position.x = x * config.ptm;
-      body.pixi.position.y = y * config.ptm;
-      body.pixi.rotation = body.b2.GetAngle();
     }, this);
     this.stepTimeoutId = setTimeout(this.step, 1000 / config.sps);
   },
 
   redraw: function () {
-    this.renderer.render(this.stage);
+    this.forceUpdate();
     this.animationFrameId = requestAnimationFrame(this.redraw);
   },
 
@@ -165,14 +114,34 @@ export default React.createClass({
     this.mouseIsDown = false;
   },
 
+  renderBody: function (body, i) {
+    var x = body.GetPosition().get_x() * config.ptm;
+    var y = body.GetPosition().get_y() * config.ptm;
+    var deg = body.GetAngle() / Math.PI * 180;
+    return (
+      <div
+        key={i}
+        style={{
+          WebkitTransform: 'translate3d(' + x + 'px, ' + y + 'px, 0) ' +
+            'rotateZ(' + deg + 'deg)',
+          position: 'absolute'
+        }}
+      >
+        🐶
+      </div>
+    );
+  },
+
   render: function () {
     return (
       <div
-        className='physics'
+        id='index'
         onMouseDown={this.onMouseDown}
         onMouseMove={this.updateMouseCoords}
         onMouseUp={this.onMouseUp}
-      />
+      >
+        {this.bodies.map(this.renderBody)}
+      </div>
     );
   }
 });
